2f65561b36609cad363a5315b18a8949c7811980
shenxiaoqiang <shenxiaoqiang@goodfatherapp.com>
2018-11-06 11:50:22 +0800

    模块儿创建.

---
+++ ours-wechat/src/main/java/com/ours/login/LoginController.java
@@ -0,0 +1,10 @@
+package com.ours.login;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by fish on 2018/11/6.
+ */
+@RestController
+public class LoginController {
+}
