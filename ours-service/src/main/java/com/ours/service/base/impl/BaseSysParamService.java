a925e85edc2daf9ed36343ed227078274e6c12f1
shenxiaoqiang <shenxiaoqiang@goodfatherapp.com>
2018-11-14 09:50:33 +0800

    新增成员功能, 标签功能.

--- ours-service/src/main/java/com/ours/service/base/impl/BaseSysParamService.java
+++ ours-service/src/main/java/com/ours/service/base/impl/BaseSysParamService.java
@@ -4,9 +4,12 @@ import com.alibaba.fastjson.JSONObject;
 import com.ours.common.constant.RedisConstant;
 import com.ours.common.util.EmptyUtil;
 import com.ours.dao.base.BaseSysParamMapper;
+import com.ours.dao.sql.SqlProvider;
 import com.ours.model.base.BaseSysParam;
 import com.ours.service.base.IBaseSysParamService;
 import com.ours.service.redis.IRedisService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +18,8 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class BaseSysParamService implements IBaseSysParamService {
+
+    private static Logger baseLog = LoggerFactory.getLogger(BaseSysParamService.class);
 
     @Autowired
     private BaseSysParamMapper baseSysParamMapper;
@@ -25,11 +30,13 @@ public class BaseSysParamService implements IBaseSysParamService {
     @Override
     public String findValueByKey(BaseSysParam params) {
         String key = RedisConstant.BASE_SYS_PARAM + "-" + params.getParamKey().toUpperCase();
+        baseLog.info("redis key is =" + key);
         String value = this.redisService.get(key);
         if (EmptyUtil.isEmpty(value)) {
             value = this.baseSysParamMapper.findValueByKey(params).getParamValue();
             this.redisService.set(key, RedisConstant.TIME_OUT, value);
         }
+        baseLog.info("redis value is =" + value);
         return value;
     }
 }
