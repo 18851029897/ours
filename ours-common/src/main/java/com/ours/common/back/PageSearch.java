39d8ae2d59aaa92fd2c7881bf3bd3356232ac7a5
shenxiaoqiang <shenxiaoqiang@goodfatherapp.com>
2018-11-21 16:11:40 +0800

    增加排序类.

---
+++ ours-common/src/main/java/com/ours/common/back/PageSearch.java
@@ -0,0 +1,27 @@
+package com.ours.common.back;
+
+/**
+ * Created by fish on 2018/11/21.
+ */
+public class PageSearch {
+
+    private Integer start;
+
+    private Integer pageSize;
+
+    public Integer getStart() {
+        return start;
+    }
+
+    public void setStart(Integer start) {
+        this.start = start;
+    }
+
+    public Integer getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
+}
