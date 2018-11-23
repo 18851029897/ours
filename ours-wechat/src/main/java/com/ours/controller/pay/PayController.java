ecb27c31c1f10c84667711e07ba32d2164591fb9
shenxiaoqiang <shenxiaoqiang@goodfatherapp.com>
2018-11-23 10:49:11 +0800

    新增支付

--- ours-wechat/src/main/java/com/ours/controller/pay/PayController.java
+++ ours-wechat/src/main/java/com/ours/controller/pay/PayController.java
@@ -1,6 +1,7 @@
 package com.ours.controller.pay;
 
 import com.ours.common.back.DataResponse;
+import com.ours.common.util.MD5Util;
 import com.ours.model.base.BaseSysParam;
 import com.ours.service.base.IBaseSysParamService;
 import org.slf4j.Logger;
@@ -10,6 +11,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.UUID;
 
 /**
  * Created by fish on 2018/11/23.
@@ -25,11 +28,25 @@ public class PayController {
 
     @RequestMapping(value = "/pay", method = RequestMethod.POST)
     @ResponseBody
-    public DataResponse pay() {
-        //WX_APP_ID
-        String appId = this.baseSysParamService.findValueByKey(new BaseSysParam());
+    public DataResponse pay(Integer flag, Integer id) {
+        String appid = this.baseSysParamService.findValueByKey(new BaseSysParam("WX_APP_ID"));
+        String mch_id = this.baseSysParamService.findValueByKey(new BaseSysParam("WX_MCH_ID"));
+        String nonce_str = UUID.randomUUID().toString().replaceAll("-", "");
+        String sign = MD5Util.MD5Encode("");
+        String body = "";
+        String out_trade_no = "";
+        String total_fee = "";
+        String spbill_create_ip = "";
+        String notify_url = "";
+        String trade_type = "JSAPI";
+        String openid = "openid";
+        return null;
+    }
 
 
+    @RequestMapping(value = "/notify", method = RequestMethod.POST)
+    @ResponseBody
+    public DataResponse wxNotify() {
         return null;
     }
 
