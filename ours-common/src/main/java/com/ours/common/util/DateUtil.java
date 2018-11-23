dae322b8278030cbe5922625db6b18ee2edc05e5
shenxiaoqiang <shenxiaoqiang@goodfatherapp.com>
2018-11-23 11:29:56 +0800

    两个日期相差的天数

--- ours-common/src/main/java/com/ours/common/util/DateUtil.java
+++ ours-common/src/main/java/com/ours/common/util/DateUtil.java
@@ -1,5 +1,7 @@
 package com.ours.common.util;
 
+import java.util.Date;
+
 /**
  * Created by fish on 2018/7/26.
  */
@@ -32,5 +34,16 @@ public class DateUtil {
     public static final String DATE_DEFAULT_SHORT_FORMAT = "yyyyMMdd";
 
 
+    /**
+     * 通过时间秒毫秒数判断两个时间的间隔
+     *
+     * @param date1
+     * @param date2
+     * @return
+     */
+    public static int differentDaysByMillisecond(Date date1, Date date2) {
+        int days = (int) ((date2.getTime() - date1.getTime()) / (1000 * 3600 * 24));
+        return days;
+    }
 
 }
